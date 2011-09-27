ca1ea2e5a70674391988f5ca74d6ce1c1fa8a805
Chris van Es <chris.vanes@openwave.com>
2011-09-27 23:56:35 +0100

    Basic plugin which doesn't do much.

--- src/main/java/com/cvanes/IncrementalTestListener.java
+++ src/main/java/com/cvanes/IncrementalTestListener.java
@@ -1,19 +1,10 @@
 package com.cvanes;
 
-import java.io.IOException;
-import java.io.PrintStream;
-
-import javax.servlet.ServletException;
-
-import org.kohsuke.stapler.AncestorInPath;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
 import hudson.scm.ChangeLogSet;
 import hudson.scm.ChangeLogSet.Entry;
 import hudson.tasks.BuildStepDescriptor;
@@ -22,57 +13,105 @@ import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
 import hudson.tasks.junit.TestResultAction;
 
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * This plugin assumes that the project has at least one unit test.
+ */
 public class IncrementalTestListener extends Recorder {
-	
+
+    private final String includes;
+
     @Extension
     public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        @Override
         public String getDisplayName() {
-            return "Fail Build When No New Tests Added";
+            return "Fail build when no new unit tests added";
         }
-        
+
         @Override
         public String getHelpFile() {
-            return "/plugin/incremental-test-plugin/help-enabled.html";
+            return "/plugin/incremental-test-plugin/help.html";
         }
 
-		@Override
-		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-			return true;
-		}
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
 
     }
-	
-	@DataBoundConstructor
-	public IncrementalTestListener() {
-		// no config as of yet
-	}
-	
-	public BuildStepMonitor getRequiredMonitorService() {
-		return BuildStepMonitor.NONE;
-	}
 
-	@Override
-	public boolean perform(AbstractBuild<?, ?> build, 
-			Launcher launcher,
-			BuildListener listener) throws InterruptedException, IOException {
-		
-		PrintStream logger = listener.getLogger();
-		
-		int total = 0;
-		TestResultAction testResults = (TestResultAction) build.getTestResultAction();
-		if (testResults != null) {
-			total = testResults.getTotalCount();
-			logger.println("Total tests : " + total);
-		} else {
-			logger.println("Naughty project with no unit tests");
-		}
-		
-		ChangeLogSet<? extends Entry> changes = build.getChangeSet();
-		// manual build with scm having been previously polled
-		if (changes.isEmptySet()) {
-			return true;
-		}
-		return true;
-	}
+    public String getIncludes() {
+        return includes;
+    }
+
+    @DataBoundConstructor
+    public IncrementalTestListener(String includes) {
+        this.includes = includes;
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build,
+                           Launcher launcher,
+                           BuildListener listener) throws InterruptedException, IOException {
+
+        PrintStream logger = listener.getLogger();
+
+        int lastCount = 0;
+        int count = 0;
+        TestResultAction previousTestResults = (TestResultAction) build.getPreviousSuccessfulBuild().getTestResultAction();
+        TestResultAction testResults = (TestResultAction) build.getTestResultAction();
+        if (previousTestResults != null) {
+            lastCount = previousTestResults.getTotalCount();
+            logger.println("Total number of tests from last successful build : " + lastCount);
+        }
+        if (testResults != null) {
+            count = testResults.getTotalCount();
+            logger.println("Total number of tests : " + count);
+        } else {
+            logger.println("No unit tests found");
+        }
+
+        ChangeLogSet<? extends Entry> changes = build.getChangeSet();
+        if (shouldCheckTestStatusFor(logger, changes)) {
+            if (count - lastCount > 0) {
+                // we have new tests!!!
+                return true;
+            }
+            // we have no new tests
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * We only want to check for new tests if files have been added or modified,
+     * not when they're deleted. We also want to check the includes and excludes
+     * lists.
+     *
+     * @param logger
+     * @param changes
+     * @return
+     */
+    private boolean shouldCheckTestStatusFor(PrintStream logger, ChangeLogSet<? extends Entry> changes) {
+        if (!changes.isEmptySet()) {
+//            for (Entry entry : changes) {
+//                for (AffectedFile file : entry.getAffectedFiles()) {
+//                    String path = file.getPath();
+//                    logger.println(path);
+//                }
+//            }
+            return true;
+        }
+        return false;
+    }
 
 }
---
+++ src/test/java/com/cvanes/IncrementalTestListenerTest.java
@@ -0,0 +1,37 @@
+package com.cvanes;
+
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.tasks.junit.TestResultAction;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class IncrementalTestListenerTest {
+
+    private final TestResultAction testResults = Mockito.mock(TestResultAction.class);
+
+    private final AbstractBuild<?, ?> build = Mockito.mock(AbstractBuild.class);
+
+    private final AbstractBuild previousBuild = Mockito.mock(AbstractBuild.class);
+
+    private final Launcher launcher = Mockito.mock(Launcher.class);
+
+    private final BuildListener listener = Mockito.mock(BuildListener.class);
+
+    private IncrementalTestListener testListener;
+
+    @Before
+    public void setUp() throws Exception {
+        testListener = new IncrementalTestListener("", "");
+    }
+
+    @Test
+    public void shouldTestSomething() throws Exception {
+        Mockito.when(build.getPreviousSuccessfulBuild()).thenReturn(previousBuild);
+        Mockito.when(build.getTestResultAction()).thenReturn(testResults);
+        testListener.perform(build, launcher, listener);
+    }
+}
